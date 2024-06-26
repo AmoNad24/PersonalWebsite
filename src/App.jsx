65c3b29cf18e8fc5686e862f181eee7b45666a4c
unknown <amoghvnadkarni001@gmail.com>
2024-06-26 23:27:23 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+function App() {
+
+  return (
+    <>
+<div className="bg-slate-900	 text-white px-2 py-2 shadow-xl rounded-2xl w-1/2 mx-auto my-3">
+
+
+     <div className="flex flex-wrap justify-between gap-x-8 ">
+      <div className="ml-5 font-mono">Amogh Nadkarni</div>
+      <div className="flex  gap-x-8 mr-5">
+     <a href="https://drive.google.com/file/d/1kjMQyHWMdB6npuADMdN4wJwXfqI9HwKm/view?usp=sharing" target="_blank">Resume</a>
+     <button>Contact Me</button>
+      </div>
+     </div>
+
+</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Content.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import photo from '../assets/Amogh-ProfPic-modified.png';
+
+export default function Content() {
+  return (
+    <>
+    <div >
+
+    <div className='flex justify-center gap-3 text-black  my-24 mx-auto '>
+      <img src={photo} alt='profpic' className='h-72 w-72'/>
+      <div className=' h-72- w-72 '>
+<br/>
+      <h1 className='font-bold	text-3xl'>Amogh Nadkarni</h1>
+    <h2 className='font-semibold	'>A bit about me</h2>
+    <p>As a dedicated Information Science and Engineering student at BMS College of Engineering, I embody
+a spirit of quick learning and excel as a team player. I aspire to be a web developer and am eager to
+contribute innovative solutions and embracing challenges.</p>
+</div>
+    </div>
+
+    </div>
+    </>
+  )
+}
