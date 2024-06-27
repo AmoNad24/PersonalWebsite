9e990e22f583fc66dcd8510c64a6318c38d8f683
unknown <amoghvnadkarni001@gmail.com>
2024-06-27 11:14:16 +0530

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-
+import React from 'react';
 
 function App() {
-
   return (
     <>
-<div className="bg-slate-900	 text-white px-2 py-2 shadow-xl rounded-2xl w-1/2 mx-auto my-3">
-
-
-     <div className="flex flex-wrap justify-between gap-x-8 ">
-      <div className="ml-5 font-mono">Amogh Nadkarni</div>
-      <div className="flex  gap-x-8 mr-5">
-     <a href="https://drive.google.com/file/d/1kjMQyHWMdB6npuADMdN4wJwXfqI9HwKm/view?usp=sharing" target="_blank">Resume</a>
-     <a href="mailto:amoghvnadkarni001@gmail.com">Contact Me</a>
-     
+      <div className="bg-slate-900 text-white px-4 py-4 shadow-xl rounded-2xl w-11/12 md:w-3/4 lg:w-1/2 mx-auto my-3">
+        <div className="flex flex-col md:flex-row justify-between gap-y-4 gap-x-8">
+          <div className="ml-5 font-mono text-center md:text-left">Amogh Nadkarni</div>
+          <div className="flex flex-col md:flex-row gap-y-4 gap-x-8 mr-5">
+            <a href="https://drive.google.com/file/d/1kjMQyHWMdB6npuADMdN4wJwXfqI9HwKm/view?usp=sharing" target="_blank">Resume</a>
+            <a href="mailto:amoghvnadkarni001@gmail.com">Contact Me</a>
+          </div>
+        </div>
       </div>
-     </div>
-
-</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-function Footer() {
+export default function Footer() {
   return (
     <>
-    <div className='flex mb-0 w-full h-36    bg-slate-900 text-white justify-around'>
-        <a href='https://www.linkedin.com/in/amogh-nadkarni-ab67482a9/' target='_blank' className='text-xl mt-3'><i class="fa-brands fa-linkedin"></i> LinkedIn</a>
-        <a href='https://github.com/AmoNad24' target='_blank' className='text-xl mt-3'><i class="fa-brands fa-github"></i> Github</a>
-        <a href='https://www.instagram.com/nadkarni_amogh/' target='_blank' className='text-xl mt-3'><i className="fa-brands fa-instagram"></i> Instagram</a>
-    </div>
-
+      <div className='flex flex-col sm:flex-row mb-0 w-full h-36 bg-slate-900 text-white justify-around items-center'>
+        <a href='https://www.linkedin.com/in/amogh-nadkarni-ab67482a9/' target='_blank' className='text-xl mt-3'>
+          <i className="fa-brands fa-linkedin"></i> LinkedIn
+        </a>
+        <a href='https://github.com/AmoNad24' target='_blank' className='text-xl mt-3'>
+          <i className="fa-brands fa-github"></i> Github
+        </a>
+        <a href='https://www.instagram.com/nadkarni_amogh/' target='_blank' className='text-xl mt-3'>
+          <i className="fa-brands fa-instagram"></i> Instagram
+        </a>
+      </div>
     </>
-  )
+  );
 }
-
-export default Footer
